782ed5cf29cdbdcd80a71d7e39045d5eb747afce
Adit0507 <adi05072002@gmail.com>
2025-07-20 22:40:24 +0530

    addin friend to user's friend list

--- api/user.go
+++ api/user.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Adit0507/nearby-friends/models"
 	"github.com/Adit0507/nearby-friends/storage"
+	"github.com/gorilla/mux"
 )
 
 func CreateUser(redisClient *storage.RedisClient, cassandraClient *storage.CassandraClient) http.HandlerFunc {
@@ -13,16 +14,37 @@ func CreateUser(redisClient *storage.RedisClient, cassandraClient *storage.Cassa
 		var user models.User
 		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
-			return 
+			return
 		}
 
 		err := redisClient.Client.HSet(r.Context(), "users", user.ID, user.Name).Err()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return 
+			return
 		}
 
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(user)
+	}
+}
+
+func AddFriend(redisClient *storage.RedisClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		userId := mux.Vars(r)["id"]
+
+		var req models.FriendRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// addin friend to user's friend list in Redis
+		err := redisClient.Client.SAdd(r.Context(), "friends: "+ userId, req.FriendID).Err()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
